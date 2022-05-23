97a89abd1c8dd5025309926e401fb5652193f938
rbhatti <robeel.bhatti@kinsaleins.com>
2022-05-23 13:43:21 -0400

    added code that publishes message to SNS topic

--- PostToSNSTopic/src/main/java/PostToSNSTopic.java
+++ PostToSNSTopic/src/main/java/PostToSNSTopic.java
@@ -2,6 +2,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.PublishRequest;
 
 
 public class PostToSNSTopic {
@@ -13,7 +14,13 @@ public class PostToSNSTopic {
                 .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .build();
 
-        String message = "This is a test message";
+        String message = "Hello from Robeel's Java App!";
+        // topicArn will have to be changed from topic playground account to topic in my personal AWS account
+        String topicArn = "arn:aws:sns:us-east-1:367780398197:Testing-Topic";
+        PublishRequest publishRequest = new PublishRequest(topicArn, message);
+
+        snsClient.publish(publishRequest);
+        System.out.println("Successfully published message to SNS Topic");
 
 
     }
